ddfe9db2cc9046a1ec21386c4075f5e2b3e00e71
Martha <mauge15@msn.com>
2017-11-26 12:01:30 +0100

    Models por m

--- views/propietario/view.php
+++ views/propietario/view.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
-use app\Models\Referencia;
+use app\models\Referencia;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Propietario */
