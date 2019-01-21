ccf00c60bd62e35ee918bfad243474d4dcead926
Martha <mauge15@msn.com>
2019-01-21 22:22:47 +0100

    Acepta cualquier raza

--- controllers/PropietarioController.php
+++ controllers/PropietarioController.php
@@ -92,6 +92,7 @@ class PropietarioController extends Controller
             Yii::debug('data saved by post');
             Yii::debug(property_exists("Raza", "id_raza"));
             //Yii::debug('Value of id Raza is '+var_export(property_exists("Raza", "id_raza")));
+            //Prueba
             if (!property_exists("Raza", "id_raza"))
             {
                     $modelRaza->save(false);
